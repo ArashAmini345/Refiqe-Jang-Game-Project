5ee24e1df283d26fb95227e21a3806ee54bbba4b
ArashAmini345 <75423754+ArashAmini345@users.noreply.github.com>
2020-12-09 16:54:52 +0330

    Add files via upload

---
+++ luanch.cs
@@ -0,0 +1,12 @@
+using vs;
+using app;
+using system editor;
+using editor;
+public class system_luanch(){
+    public void start(){
+        Run_app = Local\system
+        if not (0) = {
+            Messagebox.Show("مشکلی در اجرای برنامه بوجود آمده است.");
+        }
+    }
+}
